38197d3a9ebdd389c8fb171552ea2573194c822f
AlenS <alenskupnjak@yahoo.com>
2020-02-18 19:14:27 +0100

    001

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+defaultQuestion = 'pet';
+answer = '';
+genders = ['Muško', 'Žensko'];
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
+
+  onSubmit(form: NgForm) {
+
+    console.log(form);
+  }
+
+
 }
