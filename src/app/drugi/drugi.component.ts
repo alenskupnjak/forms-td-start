0fc683ded34e69f99bc488b88714ab202f65526a
AlenS <alenskupnjak@yahoo.com>
2020-03-03 20:47:40 +0100

    006 primjer 2

--- src/app/drugi/drugi.component.ts
+++ src/app/drugi/drugi.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-drugi',
@@ -6,8 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drugi.component.css']
 })
 export class DrugiComponent implements OnInit {
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['Muško', 'Žensko'];
+  pretplata = ['Basic', 'Advanced', 'Pro'];
+  pocetnaVrijednost = 'Advanced';
+  user = {
+    username: '',
+    email: '',
+    tajnoPitanje: '',
+    odgovor: '',
+    gender: ''
+  };
+
+  mojiPodaci = {
+    mojeEmail: '',
+    dugmad: '',
+    Password: '',
+  };
+
+  subbmited = false;
 
   constructor() { }
+
+
+  mojaForma(form: NgForm) {
+    console.log(form);
+    this.subbmited = true;
+    this.mojiPodaci.mojeEmail = form.value.mojeEmail;
+    this.mojiPodaci.dugmad = form.value.dataPretplata;
+    this.mojiPodaci.Password = form.value.pass;
+  }
+
 
   ngOnInit() {
   }
